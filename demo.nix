0226f2bd127bb92bc863feb325b56268c15e3342
Arthur Gautier <arthur.gautier@arista.com>
2021-11-25 13:38:53 -0800

    missing guids

--- demo.nix
+++ demo.nix
@@ -16,6 +16,16 @@ let
   system-image = import image-builder/verity-firmware.nix {
     inherit pkgs configuration;
     inherit (pkgs) lib;
+    innerGuid = {
+      btree = "cf102aaf-ef46-48bc-be47-54a3680da159";
+      data = "00299141-a24e-4f28-b042-414fcf8349ad";
+      kernel = "58628ff9-750a-4378-873b-6aba3e9d8c62";
+    };
+    outerGuid = {
+      esp = "12fd7b40-2a42-495e-aa3b-f1c9c3c3ad05";
+      firmwareA = "ed38b728-db62-4127-8962-9ef6ba2c78b0";
+      firmwareB = "1902c57d-d578-4ecb-8294-40859c937d4e";
+    };
   };
 in pkgs.writeShellScript "run.sh" ''
   ${pkgs.qemu}/bin/qemu-system-x86_64 \
